4260afca997fa9dd9337022c401c6c154fd675b5
lizongqi007 <1842355935@qq.com>
2016-01-20 15:58:37 +0800

    二次

--- app/src/main/java/com/gaozhen/fruitbuyer/activity/ForActivity.java
+++ app/src/main/java/com/gaozhen/fruitbuyer/activity/ForActivity.java
@@ -2,14 +2,23 @@ package com.gaozhen.fruitbuyer.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.gaozhen.fruitbuyer.R;
 
 public class ForActivity extends Activity {
 
+    private TextView mMeili;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_for);
+
+        init();
+    }
+
+    private void init() {
+        mMeili=(TextView)findViewById(R.id.meili);
     }
 }
