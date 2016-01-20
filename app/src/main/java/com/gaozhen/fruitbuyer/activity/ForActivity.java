7ad1fcd477817546265058a7540b9d2a7e3bf82d
lizongqi007 <1842355935@qq.com>
2016-01-20 16:25:09 +0800

    四次

--- app/src/main/java/com/gaozhen/fruitbuyer/activity/ForActivity.java
+++ app/src/main/java/com/gaozhen/fruitbuyer/activity/ForActivity.java
@@ -2,6 +2,7 @@ package com.gaozhen.fruitbuyer.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.gaozhen.fruitbuyer.R;
@@ -21,5 +22,11 @@ public class ForActivity extends Activity {
     private void init() {
         mMeili=(TextView)findViewById(R.id.meili);
         mMeili.setText("坚持是世界上最美的东西");
+        mMeili.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
