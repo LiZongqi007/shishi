c7506c4712d0a1a852a5c248cd3c9d3c2ad76ac4
lizongqi007 <1842355935@qq.com>
2016-01-20 15:27:35 +0800

    二次

---
+++ app/src/main/java/com/gaozhen/fruitbuyer/activity/ForActivity.java
@@ -0,0 +1,15 @@
+package com.gaozhen.fruitbuyer.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.gaozhen.fruitbuyer.R;
+
+public class ForActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_for);
+    }
+}
